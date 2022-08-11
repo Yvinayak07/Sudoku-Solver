e935d70d942f2323f610331f000b7ec76e1ded54
Yvinayak07 <100944709+Yvinayak07@users.noreply.github.com>
2022-08-11 15:04:46 +0530

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,4 @@
-// // ____DATA DEFINITIONS____ // //
 
-// Board is List[List[Int]] or false
-// interp. as a sudoku board, or false if the given board is invalid
-// EXAMPLE BOARDS
 
 const b = null
 
@@ -54,7 +50,6 @@ var bd4 = [ [1, 2, 3, 4, 5, 6, 7, 8, b],
 
 
 function initiate() {
-    // null -> null
     // populate the board with whatever the user inputted
     var startingBoard = [[]]
     var j = 0
@@ -83,8 +78,6 @@ function initiate() {
 }
 
 function solve(board) {
-    // THIS FUNCTION WORKS.
-    // Board -> Board
     // solves the given sudoku board
     // ASSUME the given sudoku board is valid
     if (solved(board)) {
@@ -97,13 +90,9 @@ function solve(board) {
     }
 }
 
-// ______TESTS______ //
-// console.log(solve(bd4))
-// ______TESTS______ //
 
 
 function searchForSolution(boards){
-    // List[Board] -> Board or false
     // finds a valid solution to the sudoku problem
     if (boards.length < 1){
         return false
@@ -123,8 +112,6 @@ function searchForSolution(boards){
 
 
 function solved(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
     // checks to see if the given puzzle is solved
     for (var i = 0; i < 9; i++){
         for (var j = 0; j < 9; j++){
@@ -136,14 +123,9 @@ function solved(board){
     return true
 }
 
-// ______TESTS______ //
-// console.log(solved(bd3))
-// ______TESTS______ //
 
 
 function nextBoards(board){ 
-    // THIS FUNCTION WORKS.
-    // Board -> List[Board]
     // finds the first emply square and generates 9 different boards filling in that square with numbers 1...9
     var res = []
     const firstEmpty = findEmptySquare(board)
@@ -162,8 +144,7 @@ function nextBoards(board){
 }
 
 function findEmptySquare(board){
-    // THIS FUNCTION WORKS.
-    // Board -> [Int, Int] 
+    
     // (get the i j coordinates for the first empty square)
     for (var i = 0; i < 9; i++){
         for (var j = 0; j < 9; j++){
@@ -174,14 +155,8 @@ function findEmptySquare(board){
     }
 }
 
-// ______TESTS______ //
-// console.log(nextBoards(bd3))
-// console.log(findEmptySquare(bd3))
-// ______TESTS______ //
 
 function keepOnlyValid(boards){
-    // THIS FUNCTION WORKS.
-    // List[Board] -> List[Board]
     // filters out all of the invalid boards from the list
     var res = []
     for (var i = 0; i < boards.length; i++){
@@ -192,21 +167,15 @@ function keepOnlyValid(boards){
     return res
 }
 
-// ______TESTS______ //
-// console.log(keepOnlyValid([bd1, bd2, bd3]))
-// ______TESTS______ //
 
 
 function validBoard(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
+    
     // checks to see if given board is valid
     return rowsGood(board) && columnsGood(board) && boxesGood(board)
 }
 
 function rowsGood(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
     // makes sure there are no repeating numbers for each row
     for (var i = 0; i < 9; i++){
         var cur = []
@@ -223,8 +192,6 @@ function rowsGood(board){
 }
 
 function columnsGood(board){
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
     // makes sure there are no repeating numbers for each column
     for (var i = 0; i < 9; i++){
         var cur = []
@@ -246,8 +213,7 @@ function boxesGood(board){
     const boxCoordinates = [[0, 0], [0, 1], [0, 2],
                             [1, 0], [1, 1], [1, 2],
                             [2, 0], [2, 1], [2, 2]]
-    // THIS FUNCTION WORKS.
-    // Board -> Boolean
+    
     // makes sure there are no repeating numbers for each box
     for (var y = 0; y < 9; y += 3){
         for (var x = 0; x < 9; x += 3){
@@ -269,34 +235,15 @@ function boxesGood(board){
     return true
 }
 
-// ______TESTS______ //
-// console.log("Rows:")
-// console.log(rowsGood(bd1))
-// console.log(rowsGood(bd2))
-// console.log(rowsGood(bd3))
-// console.log("Columns:")
-// console.log(columnsGood(bd1))
-// console.log(columnsGood(bd2))
-// console.log(columnsGood(bd3))
-// console.log("Boxes:")
-// console.log(boxesGood(bd1))
-// console.log(boxesGood(bd2))
-// console.log(boxesGood(bd3))
-// console.log("Valid?")
-// console.log(validBoard(bd1))
-// console.log(validBoard(bd2))
-// console.log(validBoard(bd3))
-// ______TESTS______ //
+
 
 
 function updateBoard(board) {
-    // THIS FUNCTION WORKS.
-    // Board -> null
     // update the DOM with the answer
     if (board == false){
-        for (i = 1; i <= 9; i++){
+//         for (i = 1; i <= 9; i++){
             document.getElementById("row " + String(i)).innerHTML = "NO SOLUTION EXISTS TO THE GIVEN BOARD"
-        }
+//         }
     }
     else{
         for (var i = 1; i <= 9; i++){
